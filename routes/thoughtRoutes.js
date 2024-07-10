b3837e5a3e5d5eac0291e87c47963cb791840b14
U6028449 <160793922+U6028449@users.noreply.github.com>
2024-07-09 21:16:02 -0600

    added route for deleting reaction

--- routes/thoughtRoutes.js
+++ routes/thoughtRoutes.js
@@ -19,7 +19,10 @@ router.put('/:id', updateThought);
 // Delete a thought by ID
 router.delete('/:id', deleteThought);
 
-// Corrected route to add a reaction to a thought
+// route to add a reaction to a thought
 router.post('/:thoughtId/reactions', reactionController.addReaction);
+
+// route to remove a reaction from a thought
+router.delete('/:thoughtId/reactions/:reactionId', reactionController.removeReaction);
 
 module.exports = router;
